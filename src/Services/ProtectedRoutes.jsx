0ccadd9c9da06c8ea05ab8881d5cab4bfad0bbf4
prathameshchatte-Biitcode <prathmesh.chatte@biitcode.com>
2024-08-20 19:56:35 +0530

    Add files via upload

---
+++ src/Services/ProtectedRoutes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+
+
+const ProtectedRoutes = () => {
+    const auth = localStorage.getItem("loggedIn");
+
+    return auth ? <Outlet/> : <Navigate to={"/login"} /> 
+
+}
+
+export default ProtectedRoutes
